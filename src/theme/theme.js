8645ba80d553d6d978700d89ab27474e41932172
Chris Ziegler <chrisrziegler@gmail.com>
2019-06-15 22:54:36 -0700

    Worked on theme/palette

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -3,12 +3,13 @@ const black = '#000';
 const darkgray = '#38474a';
 const lightgray = '#f9fafb';
 const cornsilk = 'cornsilk';
+const ltblue = '#66CFE2';
 // const teal = '#3CC1DA';
-// const orange = '#FFB615';
+const orange = '#FF5442';
 // const red = '#FF2B15';
 
 const themeLight = {
-  background: cornsilk,
+  background: ltblue,
   body: black,
   highlight: lightgray,
 };
